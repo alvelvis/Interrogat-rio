a51d0029e69deebcf60832d8eb7cdabc6dd1c45c
Elvis de Souza <elvis.desouza99@gmail.com>
2019-09-22 01:04:39 -0300

    update

--- www/cgi-bin/validar_UD.py
+++ www/cgi-bin/validar_UD.py
@@ -6,14 +6,62 @@ import pprint
 from functions import fromInterrogarToHtml
 
 def validate(conllu, sent_id = None, errorList = "validar_UD.txt"):
+
+    errorDictionary = {}
+
+    if isinstance(conllu, str):
+        corpus = estrutura_ud.Corpus(recursivo=True, sent_id=sent_id)
+        corpus.load(conllu)
+    else:
+        corpus = conllu
+
+    for sentence in corpus.sentences.values():
+        if not sentence.text.endswith(sentence.tokens[-1].word):
+            if not '1 - Sentença não termina com o último token' in errorDictionary:
+                errorDictionary['1 - Sentença não termina com o último token'] = []
+            errorDictionary['1 - Sentença não termina com o último token'].append({
+                "sentence": "",
+                "sent_id": sentence.sent_id,
+            })
+
+        temRoot = False
+        tem2Root = False
+        for token in sentence.tokens:
+            if token.deprel == "root" and not temRoot:
+                temRoot=True
+            elif token.deprel == "root" and temRoot:
+                tem2Root=True
+        if tem2Root:
+            if not '1 - Tem 2 root' in errorDictionary:
+                errorDictionary['1 - Tem 2 root'] = []
+            errorDictionary['1 - Tem 2 root'].append({
+                "sentence": "",
+                "sent_id": sentence.sent_id,
+            })
+        if not temRoot:
+            if not '1 - Não tem root' in errorDictionary:
+                errorDictionary['1 - Não tem root'] = []
+            errorDictionary['1 - Não tem root'].append({
+                "sentence": "",
+                "sent_id": sentence.sent_id,
+            })
+
+    missingToken = re.search(r"\n\n(?!# text)", conllu.to_str())
+    if missingToken:
+        if not '1 - Há tokens faltando no corpus' in errorDictionary:
+            errorDictionary['1 - Há tokens faltando no corpus'] = []
+        errorDictionary['1 - Há tokens faltando no corpus'].append({
+            "sentence": "",
+            "sent_id": str(missingToken),
+        })
+
     with open(errorList) as f:
         errorListFile = f.read().splitlines()
         errorList = []
         [errorList.append(x) for x in errorListFile]
 
-    errorDictionary = {}
     for error in errorList:
-        if error:
+        if error and error[0] != "#":
             if "erro: " in error:
                 comment = error.split("erro: ")[1]
                 comment = comment.strip()
@@ -36,6 +84,7 @@ def validate(conllu, sent_id = None, errorList = "validar_UD.txt"):
                     "sentence": sentence,
                     "attribute": coluna,
                 })
+
 
     return errorDictionary
 
