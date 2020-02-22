301faabd860cf75ff280a9b64d31b56f45a2f81d
Elvis de Souza <elvis.desouza99@gmail.com>
2020-02-22 14:56:20 -0300

    update

--- run_interrogatorio.sh
+++ run_interrogatorio.sh
@@ -2,6 +2,13 @@ if [ ! -e cgi-bin ]; then
   sudo ln -rs www/interrogar-ud/ .; sudo ln -rs www/cgi-bin/ .; sudo chmod -R a+rwx www
 fi
 
+if ! python3 -c "import regex"; then
+  if ! pip3 install -r requirements.txt; then
+    sudo apt install python3-pip
+    pip3 install -r requirements.txt
+  fi
+fi
+
 if [ -d .git ]; then
   git update-index --assume-unchanged www/cgi-bin/credenciar.py
   git update-index --assume-unchanged www/cgi-bin/variables.py
