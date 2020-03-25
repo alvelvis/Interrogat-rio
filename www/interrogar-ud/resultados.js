2b6e58810d0f5b603b3444781aab1a6138e3b28e
Elvis de Souza <elvis.desouza99@gmail.com>
2020-03-24 23:46:35 -0300

    new update description

--- www/interrogar-ud/resultados.js
+++ www/interrogar-ud/resultados.js
@@ -1,4 +1,11 @@
 var translations = {
+	'Ver documentação': {
+		'en-US': 'Go to documentation'
+	},
+	'Nova função: modificar tokenização de uma sentença': {
+		'pt-BR': 'Nova função: modificar tokenização de uma sentença dentro de um inquérito',
+		'en-US': 'New function: modify a sentence tokenization inside an inquiry'
+	},
 	'Limpar expressão de busca': {
 		'en-US': 'Clear query expression'
 	},
