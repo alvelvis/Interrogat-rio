d085b1647a3dcb9aeb40402a39502f9cb03cebf0
Elvis de Souza <elvis.desouza99@gmail.com>
2020-05-17 23:01:36 -0300

    update

--- www/interrogar-ud/resultados.js
+++ www/interrogar-ud/resultados.js
@@ -1066,41 +1066,39 @@ function carregarPosts(){
     );
 };
 
+if ($('.indexSentences').length){
+	window.addEventListener('focus', function(){
+		var url;
+		if ($('[name=nome_interrogatorio]').val() == 'Busca rápida'){
+			url = '../cgi-bin/api.py'
+		} else {
+			url = '../../cgi-bin/api.py'
+		};
+		$.post(url, {
+				'indexSentences': $('.indexSentences').val(),
+				'filtrado': $('.filtrado').val(),
+				'nomePesquisa': $('[name=nome_interrogatorio]').val(),
+				'html': $('[name=link_interrogatorio]').val(),
+				'conllu': $('[name=conllu]').val(),
+				'parametros': $('#expressao').text(),
+				'script': $('[name=queryScript]').val(),
+			},
+			function(data) {
+				$('.filtrar_filtros').html(JSON.parse(data).filtrar_filtros);
+				if (JSON.parse(data).pagina_filtros.length) {
+					$('.pagina_filtros').html('<li><h4>Filtros (' + JSON.parse(data).filtros +  '):</h4></li>' + JSON.parse(data).pagina_filtros);
+				} else {
+					$('.pagina_filtros_br').remove();
+				};
+				updateTranslation();
+			},
+			"text",
+		);
+	});
+};
+
 function scrollPosts(){
 	if ($('.indexSentences').length){
-
-		window.addEventListener('focus', function(){
-	
-			$(window).unbind('scroll');
-			$("title").text("Carregando | " + $("title").text());
-			var url;
-			if ($('[name=nome_interrogatorio]').val() == 'Busca rápida'){
-				url = '../cgi-bin/api.py'
-			} else {
-				url = '../../cgi-bin/api.py'
-			};
-			$.post(url, {
-					'indexSentences': $('.indexSentences').val(),
-					'filtrado': $('.filtrado').val(),
-					'nomePesquisa': $('[name=nome_interrogatorio]').val(),
-					'html': $('[name=link_interrogatorio]').val(),
-					'conllu': $('[name=conllu]').val(),
-					'parametros': $('#expressao').text(),
-					'script': $('[name=queryScript]').val(),
-				},
-				function(data) {
-					$('.filtrar_filtros').html(JSON.parse(data).filtrar_filtros);
-					if (JSON.parse(data).pagina_filtros.length) {
-						$('.pagina_filtros').html('<li><h4>Filtros (' + JSON.parse(data).filtros +  '):</h4></li>' + JSON.parse(data).pagina_filtros);
-					} else {
-						$('.pagina_filtros_br').remove();
-					};
-					updateTranslation();
-				},
-				"text",
-			);
-		});
-
 		$(window).scroll(function() {    
 			if($(window).scrollTop() + $(window).height() > $(document).height() -5000) {
 				$(window).unbind('scroll');
