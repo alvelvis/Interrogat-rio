e72ddea026fa198d1e6802e99545ac50a27d3440
Elvis de Souza <elvis.desouza99@gmail.com>
2020-09-13 15:25:50 -0300

    update

--- install_interrogatorio.sh
+++ install_interrogatorio.sh
@@ -1,5 +1,6 @@
 if [ ! -d .interrogatorio ]; then
     if ! virtualenv .interrogatorio -p python3; then
+        sudo apt update
         sudo apt install virtualenv
         virtualenv .interrogatorio -p python3
     fi
