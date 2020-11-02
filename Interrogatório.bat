996c97b9e3ae9e69a7e7ea3a4ac4d8424d7ff247
Elvis de Souza <elvis.desouza99@gmail.com>
2020-11-01 23:20:21 -0300

    Update Interrogatório.bat

--- Interrogatório.bat
+++ Interrogatório.bat
@@ -1,30 +1,27 @@
 @ECHO OFF
-ubuntu run exit >nul 2>&1 && (
-    if exist Interrogat-rio (
-      del /f Interrogatorio_ini
-      start "" ubuntu run LANG="en_US.UTF-8"; cd Interrogat-rio; sh run_interrogatorio.sh
-      start "" http://localhost:8000
-      exit
-    ) else ( 
-      ubuntu run "git clone https://github.com/alvelvis/Interrogat-rio"
-      ubuntu run "cd Interrogat-rio; sh install_interrogatorio.sh"
-      start "" ubuntu run LANG="en_US.UTF-8"; cd Interrogat-rio; sh run_interrogatorio.sh
-      start "" http://localhost:8000
-      exit
+ubuntu run exit && (
+  if exist Interrogat-rio (
+    del /f C:\Interrogatorio_ini
+    start "" ubuntu run LANG="en_US.UTF-8"; cd Interrogat-rio; sh run_interrogatorio.sh
+    start "" http://localhost:8000
+    exit
+  ) else ( 
+    start "" ubuntu run "git clone https://github.com/alvelvis/Interrogat-rio; cd Interrogat-rio; sh install_interrogatorio.sh"
+    exit
+  )
+)||(
+  if exist C:\Interrogatorio_ini (
+    start "" https://www.microsoft.com/pt-br/p/ubuntu/9nblggh4msv6
+    exit
+  ) else (
+    net session >nul 2>&1 && (
+      powershell -Command "dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart"
+      echo WSL enabled> C:\Interrogatorio_ini
+      echo Computer rebooting in a few seconds...
+      shutdown /r
+    )||(
+      ECHO Run this as administrator for the first time.
     )
-) || (
-    if exist Interrogatorio_ini (
-      start "" https://www.microsoft.com/pt-br/p/ubuntu/9nblggh4msv6
-      exit
-    ) else (
-    NET SESSION >nul 2>&1
-    IF %ERRORLEVEL% EQU 0 (
-        powershell -Command "dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart"
-        echo WSL enabled> Interrogatorio_ini
-        shutdown /r
-    ) ELSE (
-        ECHO Run this as administrator for the first time.
-        exit
-    )
+  )
 )
 pause
