d8a2adc96ba77aed4e114ada723e66bf6930a70e
myusername <myemail>
2021-09-23 06:03:47 -0300

    only need to find a packager

--- interrogatorio_for_windows.py
+++ interrogatorio_for_windows.py
@@ -7,21 +7,30 @@ import sys
 import subprocess
 import webbrowser
 import shutil
+import socket
 from git import Repo
 from http.server import HTTPServer, CGIHTTPRequestHandler
 
-path = os.path.dirname(os.path.abspath(__file__))
-repo = Repo('{}'.format(path))
-repo.config_writer().set_value("user", "name", "myusername").release()
-repo.config_writer().set_value("user", "email", "myemail").release()
-repo.git.pull()
+def main():
+    os.environ['PYTHONUTF8'] = "1"
+    path = os.path.dirname(os.path.abspath(__file__))
+    repo = Repo('{}'.format(path))
+    repo.config_writer().set_value("user", "name", "myusername").release()
+    repo.config_writer().set_value("user", "email", "myemail").release()
+    repo.git.pull()
+    
+    sys.stderr.write("\n=== INTERROGATÓRIO ===\n\n>>> Open 'http://localhost:8000' on your browser to access Interrogatório locally.\n")
 
-os.environ['PYTHONUTF8'] = "1"
-print("\n=== INTERROGATÓRIO ===\n\n>>> Open 'http://localhost:8000' on your browser to access Interrogatório locally.\n")
+    def is_port_in_use(port):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            return s.connect_ex(('localhost', port)) == 0
 
-os.chdir(os.path.join(os.path.dirname(os.path.abspath(__file__)), "www"))
-CGIHTTPRequestHandler.cgi_directories = ['/cgi-bin']
-httpd = HTTPServer(('', 8000), CGIHTTPRequestHandler)
+    if not is_port_in_use(8000):
+        os.chdir(os.path.join(os.path.dirname(os.path.abspath(__file__)), "www"))
+        server_object = HTTPServer(server_address=('', 8000), RequestHandlerClass=CGIHTTPRequestHandler)
+        webbrowser.open('http://localhost:8000')
+        server_object.serve_forever()
+    else:
+        sys.stderr.write("\n\nPort 8000 already in use.")
 
-webbrowser.open('http://localhost:8000')
-httpd.serve_forever()
+main()
