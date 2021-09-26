99d9314355070e6368889e8e3d6f00a3566f5797
Elvis de Souza <elvis.desouza99@gmail.com>
2021-09-26 15:34:53 -0300

    improved bat

--- interrogatorio_for_windows.bat
+++ interrogatorio_for_windows.bat
@@ -1,3 +1,7 @@
 @ECHO OFF
-Python39\Python.exe for_windows.py
+if exist Python39\ (
+    Python39\Python.exe for_windows.py
+) else (
+    echo Python39 not found, please download the Windows release from https://github.com/alvelvis/Interrogat-rio and extract the folder from the zip file.
+)
 pause
