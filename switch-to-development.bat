569d539e24b8dcf47f1994c6af63432beb5dc016
Elvis de Souza <elvis.desouza99@gmail.com>
2023-07-18 21:13:11 -0300

    add switch-to-development launcher

---
+++ switch-to-development.bat
@@ -0,0 +1,8 @@
+@ECHO OFF
+if exist Python39\ (
+    echo 
+    Python39\Python.exe for_windows.py development
+) else (
+    echo Python39 not found, please download the Windows release from https://github.com/alvelvis/Interrogat-rio and extract the folder from the zip file.
+)
+pause
