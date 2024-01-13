8011502f616f3be186f8d7e5da2b634d123ec659
Elvis de Souza <elvis.desouza99@gmail.com>
2024-01-13 17:22:08 -0300

    doc: instruções mais claras para Windows

--- interrogatorio_for_windows.bat
+++ interrogatorio_for_windows.bat
@@ -2,6 +2,8 @@
 if exist Python39\ (
     Python39\Python.exe for_windows.py
 ) else (
-    echo Python39 not found, please download the Windows release from https://github.com/alvelvis/Interrogat-rio and extract the folder from the zip file.
+    echo There are missing files in this Windows release of Interrogatorio.
+    echo 1. Please download the Windows release from https://github.com/alvelvis/Interrogat-rio/releases
+    echo 2. Make sure to open Interrogatorio from the file interrogatorio_for_windows only after extracting the zipped downloaded file Interrogat-rio.zip to a folder of your choice.
 )
 pause
